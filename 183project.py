f26490438edaba6b0d0b54cf48085437bce541ea
haalbright <67480690+haalbright@users.noreply.github.com>
2022-03-13 18:23:58 -0700

    Add files via upload

--- 183project.py
+++ 183project.py
@@ -9,21 +9,15 @@ def readCSV(iFile, cols):
 sDeaths=readCSV(PATH+"Medical_Examiner-Coroner__Suicide_Deaths_dataset.csv", ["Death Date", "Age", "Race", "Gender"])
 sDeaths['Death Date']=pd.to_datetime(sDeaths['Death Date'])
 sDeaths.set_index('Death Date', inplace=True)
+sDeaths.index=sDeaths.index.normalize()
+sDeaths['Race'].replace(['White', 'Hispanic', 'Asian', 'OtherPacificIslander', 'Other','BlackAfricanAmerican', 'Unknown', 'American Indian'], range(0,8), inplace=True)
+sDeaths['Gender'].replace(['Male', 'Female', None], [0,1,2], inplace=True)
 sDeaths_2020=sDeaths['2020-02':'2021-01-31']
 sDeaths_2019=sDeaths['2019-01':'2019-12-31']
-s2020=pd.get_dummies(sDeaths_2020)
-sresample=s2020.drop("Age", axis=1).resample("M").sum()
-sresample['Avg_Age']=s2020['Age'].resample("M").mean()
-
+sDeaths_2019.to_csv(PATH+"sDeaths2019.csv")
 cCases=readCSV(PATH+"COVID-19_case_counts_by_date.csv",["Date", "Total_cases", "New_cases"])
 cCases['Date']=pd.to_datetime(cCases['Date'])
 cCases.set_index('Date', inplace=True)
 c2020=cCases['2020-02':'2021-01-31']#first covid case documented on 2020/1/27 so start at the beginning of the next month
-cresample=c2020.drop("Total_cases", axis=1).resample("M").sum()
-df_2020=cresample.join(sresample)
-df_2020.to_csv(PATH+"s2020.csv")
-
-s2019=pd.get_dummies(sDeaths_2019)
-sresample2=s2019.drop("Age", axis=1).resample("M").sum()
-sresample2['Avg_Age']=s2019['Age'].resample("M").mean()
-sresample2.to_csv(PATH+"s2019.csv")
+df_2020=sDeaths_2020.join(c2020['Total_cases'], how='left')
+df_2020.to_csv(PATH+"sDeaths2020.csv")
