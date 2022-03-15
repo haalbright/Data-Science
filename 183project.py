55694857418865f244733b09467bb536cf9f1c2e
haalbright <67480690+haalbright@users.noreply.github.com>
2022-03-14 22:13:41 -0700

    Add files via upload

--- 183project.py
+++ 183project.py
@@ -7,22 +7,18 @@ def readCSV(iFile, cols):
     df=pd.read_csv(iFile, header=0, delimiter=",", usecols=cols)
     return df
 
-def plotGraphs(df, target): #scatter and line
-    for label in df.drop(target, axis=1).columns:
-        if df[label].dtype in ['int64', 'float64']:
-            df.boxplot(column=target, by=label)
-            name = PATH+target+"/"+label + "graph.png"
-            plt.savefig(name)
-            plt.close()
-
 sDeaths=readCSV(PATH+"Medical_Examiner-Coroner__Suicide_Deaths_dataset.csv", ["Death Date", "Age", "Race", "Gender"])
 sDeaths['Death Date']=pd.to_datetime(sDeaths['Death Date'])
 sDeaths.set_index('Death Date', inplace=True)
 sDeaths.index=sDeaths.index.normalize()
-sDeaths['Race'].replace(['White', 'Hispanic', 'Asian', 'OtherPacificIslander', 'Other','BlackAfricanAmerican', 'Unknown', 'American Indian'], range(0,8), inplace=True)
+sDeaths['Race'].replace(['White', 'Hispanic', 'Asian', 'OtherPacificIslander', 'Other','BlackAfricanAmerican', 'Unknown', 'American Indian'], [0,1,2,4,5,3,6,7], inplace=True)
 sDeaths['Gender'].replace(['Male', 'Female', None], [0,1,2], inplace=True)
 sDeaths_2020=sDeaths['2020-02':'2021-01-31']
 sDeaths_2019=sDeaths['2019-01':'2019-12-31']
+
+s2020=pd.get_dummies(sDeaths_2020)
+sresample=s2020.drop("Age", axis=1).resample("M").sum()
+sresample['Avg_Age']=s2020['Age'].resample("M").mean()
 
 #line graph of monthly suicide coutns. Blue=during covid Green=before covid
 sresample=sDeaths_2020["Gender"].resample("M").count() #sresample is a count of suicides per month in 2020
@@ -50,11 +46,7 @@ df4=sDeaths_2019.index.to_frame(index=False)
 sDeaths_2019['tsuicides_2019']=df4.index+1
 sDeaths_2019.to_csv(PATH+"sDeaths2019_count.csv")
 
-#box and whisker plots
-plotGraphs(sDeaths_2019.drop("Age", axis=1), "tsuicides_2019")
-plotGraphs(df_2020.drop(["Age", "Total_cases"], axis=1), "tsuicides_2020")
-
-#scatter plots for 
+#scatter plots for
 ax1=df_2020.plot.scatter(y="tsuicides_2020", x="Age", color="b", marker="^", alpha=0.7)
 sDeaths_2019.plot.scatter(y="tsuicides_2019", x="Age", color="g", ax=ax1, alpha=0.7)
 plt.savefig( PATH +"suicidesAgegraph.png")
